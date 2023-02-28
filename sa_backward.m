0c7b2ace4a9ce09d194a4475b84847d6220c5e72
Peter Tso <tsopeter@ku.edu>
2023-02-28 13:16:36 -0600

    Update

--- sa_backward.m
+++ sa_backward.m
@@ -2,10 +2,6 @@ function z = sa_backward(E)
     a0 = 20;
     g  = E;
 
-    if (g < 0.0001)
-        z = single(0.0001);
-    else
-        q = abs(sa_forward(E))/g;
-        z = single(q * exp(1+(a0*g*g)/(1+g^2)^2));
-    end
+    m = exp(-a0/2/(1+g^2));
+    z = single(m * exp(1+(a0*g*g)/(1+g^2)^2));
 end
--- sa_forward.m
+++ sa_forward.m
@@ -1,5 +1,5 @@
 function z = sa_forward(E)
     a0 = 20;
     g = E;
-    z = exp(-a0/2/(1+g^2));
+    z = exp(-a0/2/(1+g^2))*g;
 end
